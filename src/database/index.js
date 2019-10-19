33489dd52fa5bf0d2aac4425df28c2e55699dbce
Samuel Lucas <samuellucas0603@gmail.com>
2019-10-19 01:58:05 -0300

    create adrress get/post

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const dbconfig = require("../config/database");
 
-const User = require("../models/User");
-
 const connection = new Sequelize(dbconfig);
 
+const User = require("../models/User");
+const Address = require("../models/Address");
+
 User.init(connection);
+Address.init(connection);
+
+User.associate(connection.models);
+Address.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ const routes = express.Router();
 
 // import controllers
 const UserController = require("./controllers/UserController");
+const AddressesController = require("./controllers/AddressesController");
 
 // rotas usuarios
 routes.get("/users", UserController.index);
 routes.post("/users", UserController.store);
+
+// rotas address
+routes.post("/users/:user_id/addresses", AddressesController.store);
+routes.get("/users/:user_id/addresses", AddressesController.index);
 
 module.exports = routes;
