106f32d3184eae27b7727773161ed2bcd46afe2d
Samuel Lucas <samuellucas0603@gmail.com>
2019-10-19 00:05:13 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+     dialect: 'postgres',
+     host: 'localhost',
+     // port: '5432',
+     username: 'root',
+     password: 'eunaoseiminhasenha',
+     database: 'sqlnode',
+     define: {
+          timestamps: true,
+          underscored: true,
+     },
+}
