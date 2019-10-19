dc7b99acf56d6a92ae30820180577df509d6421c
Samuel Lucas <samuellucas0603@gmail.com>
2019-10-19 00:53:12 -0300

    function getall user

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 // import controllers
 const UserController = require("./controllers/UserController");
 
+// rotas usuarios
+routes.get("/users", UserController.index);
 routes.post("/users", UserController.store);
 
 module.exports = routes;
