5401a62104b44e969115d03786ab938f914dceb2
Samuel Lucas <samuellucas0603@gmail.com>
2019-10-19 00:48:23 -0300

    function create user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
-
+const express = require("express");
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ hello: 'oi'});
-})
+// import controllers
+const UserController = require("./controllers/UserController");
+
+routes.post("/users", UserController.store);
 
 module.exports = routes;
